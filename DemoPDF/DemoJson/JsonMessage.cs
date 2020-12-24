0cfcdd1bf1dc654c1bcdb744e374ab34eb62c2c6
Daniel Graniso Marconi <danielgmarconi@gmail.com>
2020-12-24 14:44:18 -0300

    .
    .

---
+++ DemoPDF/DemoJson/JsonMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace DemoJson
+{
+    public class JsonMessage
+    {
+        public List<JsonMessageEntity> Obter(string localFile, Nullable<int> id, string tipo)
+        {
+            List<JsonMessageEntity> list = JsonConvert.DeserializeObject<List<JsonMessageEntity>>(System.IO.File.ReadAllText(localFile));
+            return(from L in list where ((id == null ? id == null : L.id == id) && (tipo == null ? tipo == null : L.tipo.ToLower() == tipo.ToLower())) select L).ToList<JsonMessageEntity>();
+        }
+    }
+}
---
+++ DemoPDF/DemoJson/JsonMessageEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoJson
+{
+    public class JsonMessageEntity
+    {
+        public Nullable<int> id { get; set; }
+        public string tipo { get; set; }
+        public string mensagem { get; set; }
+    }
+}
