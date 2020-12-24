f1bc174c7ea940ddc7f14380a197cb9e9cc27aa6
Daniel Graniso Marconi <danielgmarconi@gmail.com>
2020-12-24 17:50:07 -0300

    .
    .

--- DemoPDF/DemoJson/JsonMessage.cs
+++ DemoPDF/DemoJson/JsonMessage.cs
@@ -7,12 +7,10 @@ namespace DemoJson
 {
     public class JsonMessage
     {
-        public static List<JsonMessageEntity> Obter(string localFile, int id, string tipo = "V")
+        public static JsonMessageEntity Obter(string localFile, int id, string tipo = "V")
         {
             List<JsonMessageEntity> list = JsonConvert.DeserializeObject<List<JsonMessageEntity>>(System.IO.File.ReadAllText(localFile));
-            return(from L in list where (L.id.Equals(id) && L.tipo.Equals(tipo)) select L).ToList<JsonMessageEntity>();
-
-            //string.Equals(StringA, StringB, StringComparison.CurrentCultureIgnoreCase);
+            return(from L in list where (L.id.Equals(id) && L.tipo.Equals(tipo, StringComparison.CurrentCultureIgnoreCase)) select L).ToList<JsonMessageEntity>().FirstOrDefault();
         }
     }
 }
